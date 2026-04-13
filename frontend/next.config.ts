ced4d237a13d5954829e66e518b53a0d50acc29d
Mohamed <mohamedal2min-u@gmail.com>
2026-04-13 22:10:14 +0200

    chore: update next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,8 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: 'http', hostname: 'localhost' },
+      { protocol: 'http', hostname: '192.168.0.210' },
     ],
   },
+  allowedDevOrigins: ['192.168.0.210'],
 }
 
 export default nextConfig
